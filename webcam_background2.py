bfbf93b32ca39cd03dabfdc367b17f62f034303e
Yushan Wang <yushanwang0816@gmail.com>
2023-04-01 12:41:34 +0800

    canny

--- webcam_background2.py
+++ webcam_background2.py
@@ -5,7 +5,21 @@ cam = cv2.VideoCapture(0)
 while True:
     ret, frame = cam.read()
 
-    cv2.imshow("imgStacked", frame)
+    gray = cv2.cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    edges = cv2.Canny(blur, 50, 150)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+
+    contours, _ = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    largest_contour = max(contours, key=cv2.contourArea)
+
+    mask = np.zeros_like(frame)
+    cv2.drawContours(mask, [largest_contour], 0, (255, 255, 255), -1)
+    segmented_selfie = cv2.bitwise_and(frame, mask)
+
+    cv2.imshow("imgStacked", edges)
     if cv2.waitKey(1) == ord('q'):  # quit if the 'q' key is pressed
         break
 
